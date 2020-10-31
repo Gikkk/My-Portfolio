aa69dd62d2ef7ac9ba426fe647a873e46289765c
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2020-10-31 12:07:27 +0400

    about

--- src/app/homepage/carousel/carousel.component.ts
+++ src/app/homepage/carousel/carousel.component.ts
@@ -10,12 +10,6 @@ export class CarouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // function classToggle() {
-    //   var el = document.querySelector('.icon-cards__content');
-    //   el.classList.toggle('step-animation');
-    // }
 
-    // document.querySelector('#toggle-animation').addEventListener('click', classToggle);
   }
-
 }
